11306f427586d7cc801ab8c9ef4466d59e23f46d
Matias Bue <matias.bue@outlook.com>
2022-10-31 18:14:12 -0300

    agregada la seccion de accdb y sentencias sql necesarias

--- Back-End/4/4.py
+++ Back-End/4/4.py
@@ -14,55 +14,45 @@ print(" -------- SQL Config Setup -------- ")
 mydb = mysql.connector.connect(
   host="localhost",
   user=input("Ingrese nombre de usuario: "),
-  password = input("Ingrese contraseña: ")
+  password = input("Ingrese contraseña: "),
+  database = "numeros"
 )
 
 cursor = mydb.cursor()
 
 # ---------------------------------------------------
 
+# ------------- Configuracion accdb -----------------
+
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ= C:\Users\_mNb\codes\python\LdL2022\Back-End\4\db.accdb;')
+cursor2 = conn.cursor()
+
+#----------------------------------------------------
+
 # Console Clear
 print("\033[H\033[J", end="")
 
 
 total = int(input("Ingrese total de localidades: "))
-loc = []
 
+# Creacion de un DataFrame para la posterior creacion del archivo .accdb
+
+
+# Conseguir N numeros de la localidad ingresada
 for i in range(total):
-    loc[i] = input("Ingrese localidad ", i+1, " :")
+    loc = input("Ingrese localidad: ")
+    query ="SELECT numero FROM telefonos WHERE nombre_localidad = %s LIMIT %s".format(loc)
+    values = (loc, round(7000/total))
+    cursor.execute(query, values,)
 
 
+for row in cursor.fetchall():
+  cursor2.execute("insert into numeros(numero) values (?)", row)
+  cursor2.commit()
 
-locs = tuple(loc)
-query ="SELECT numero FROM numeros WHERE localidad IN ({})".format(locs), " LIMIT %s";
-cursor.execute(query,7000)
-
-numero = cursor.fetchall()
-
-
-
-
-
-# Conexion al archivo Access
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\_mNb\codes\python\LdL2022\Back-End\4\db.accdb;')
-
-print("Conectado a la base de datos")
-sleep(2)
-
-
-cursor = conn.cursor()
-
-#Insert en el archivo
-for i in range(len(numero)):
-    cursor.execute("INSERT INTO numeros values (?)", numero[i])
-
-# Commit al archivo
-conn.commit()
-
-print("Numeros Agregados")
 
 # Cierre de conexion
 cursor.close()
-conn.close()
+cursor2.close()
 
 print("Finalizado Exitosamente")
