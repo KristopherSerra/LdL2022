a5a6bb2b4c44ae4847edba7adee564df40c4e10d
Matias Bue <matias.bue@outlook.com>
2022-11-09 20:24:53 -0300

    modificacion para seleccionar cantidad de localidades a cargar

--- Back-End/3/3.py
+++ Back-End/3/3.py
@@ -14,7 +14,7 @@ mydb = mysql.connector.connect(
   host="localhost",
   user=input("Ingrese nombre de usuario: "),
   password = input("Ingrese contraseña: "),
-  database = 'dannafox-test'
+  database = input("Ingrese nombre de la base de datos: ")
 )
 
 mycursor = mydb.cursor()
@@ -25,57 +25,59 @@ print("\033[H\033[J", end="")
 #Export del archivo que contiene los rangos
 rangos = pd.read_csv('rangos.csv')
 
+cantidad = input("Ingrese total de localidades a cargar: ")
 
-# Creacion del DataFrame con la info de la localidad pedida
-loc = str(input('Ingrese localidad: ').upper())
+for i in range(cantidad):
+    # Creacion del DataFrame con la info de la localidad pedida
+    loc = str(input('Ingrese localidad: ' , i+1).upper())
 
-# Chequeo para verificar si la ciudad es valida
-if (loc in set(rangos["LOCALIDAD"])): 
-    location = rangos[rangos['LOCALIDAD'] == loc]
-else:
-    print("No se ha encontrado la localidad ingresada, Finalizando...")
-    quit()
-
-
-# Contienen el conjunto del codigo de area + el bloque
-codArea = []
-bloque = [] 
-
-for i in range(len(location)):
-    codArea.append(str(location.iloc[i].INDICATIVO))
-    bloque.append(str(location.iloc[i].BLOQUE))
-print("Datos de la locacion cargados exitosamente")    
-
-# Almacena cuantos digitos faltan para completar el numero
-faltantes = [] 
-for i in range(len(bloque)):
-    faltantes.append(10 - (len(str(codArea[i])) + len(str(bloque[i]))))
-
-print("Se han realizado todos los pre-calculos necesarios")
-sleep(3)
-print("Iniciando la creacion, aguarde un momento...")
-
-
-# Crear el numero
-numeros = []
-for i in range(len(faltantes)):
-
-    start = int(bloque[i])*(10**faltantes[i])
-    if (int(bloque[i]) >= 100):
-        end = ((int(bloque[i])+1)*(10**faltantes[i]))
+    # Chequeo para verificar si la ciudad es valida
+    if (loc in set(rangos["LOCALIDAD"])): 
+        location = rangos[rangos['LOCALIDAD'] == loc]
     else:
-        end = ((int(bloque[i])+10)*(10**faltantes[i]))
+        print("No se ha encontrado la localidad ingresada, Finalizando...")
+        quit()
+
+
+    # Contienen el conjunto del codigo de area + el bloque
+    codArea = []
+    bloque = [] 
+
+    for i in range(len(location)):
+        codArea.append(str(location.iloc[i].INDICATIVO))
+        bloque.append(str(location.iloc[i].BLOQUE))
+    print("Datos de la locacion cargados exitosamente")    
+
+    # Almacena cuantos digitos faltan para completar el numero
+    faltantes = [] 
+    for i in range(len(bloque)):
+        faltantes.append(10 - (len(str(codArea[i])) + len(str(bloque[i]))))
+
+    print("Se han realizado todos los pre-calculos necesarios")
+    sleep(3)
+    print("Iniciando la creacion, aguarde un momento...")
+
+
+    # Crear el numero
+    numeros = []
+    for i in range(len(faltantes)):
+
+        start = int(bloque[i])*(10**faltantes[i])
+        if (int(bloque[i]) >= 100):
+            end = ((int(bloque[i])+1)*(10**faltantes[i]))
+        else:
+            end = ((int(bloque[i])+10)*(10**faltantes[i]))
 
 
 
-    for j in range(start, end):
-        sql = "INSERT INTO telefonos (nombre_localidad, numero) VALUES (%s, %s)".format(loc)
+        for j in range(start, end):
+            sql = "INSERT INTO telefonos (nombre_localidad, numero) VALUES (%s, %s)".format(loc)
 
-        num = str(codArea[i]) + str(j)
-        values = (loc, num,)
-        mycursor.execute(sql, values)
-        mydb.commit()
+            num = str(codArea[i]) + str(j)
+            values = (loc, num,)
+            mycursor.execute(sql, values)
+            mydb.commit()
 
-print("Numeros cargados correctamente, finalizando...")
+    print("Numeros cargados correctamente")
 
 
