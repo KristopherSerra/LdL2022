ad186fc678ac9da60b964421eddd42bd20ffd089
KristopherSerra <105523175+KristopherSerra@users.noreply.github.com>
2022-11-15 20:34:38 -0300

    Finalizacion del Ejercicio 5

--- Back-End/5/5.py
+++ Back-End/5/5.py
@@ -1,45 +1,75 @@
-import pandas as pd
+import openpyxl
 from openpyxl import load_workbook
-from openpyxl.chart import BarChart,Reference
-import string
+import datetime 
+from time import sleep
+import mysql.connector
 
-informes = pd.read_excel("BDE.xlsx")
-tabla_pivote = informes.pivot_table(index="MODALIDAD",columns='OPERADOR', values='BLOQUE')
-tabla_pivote.to_excel('Informe Automatico - Danna Fox.xlsx', startrow=1,startcol=1,sheet_name='Octubre 2022')
 
-#Cargar Libro Exce
-lectura = load_workbook('Informe Automatico - Danna Fox.xlsx')
-pestaña = lectura['Octubre 2022']
-min_columna = lectura.active.min_column
-max_columna = lectura.active.max_column
-min_fila = lectura.active.min_row
-max_fila = lectura.active.max_row
 
-#Grafico de Barras
-graficoDeBarras = BarChart()
-data = Reference(pestaña, min_col=min_columna+1,max_col=max_columna, min_row=min_fila,max_row=max_fila)
-categorias = Reference(pestaña, min_col=min_columna,max_col=min_columna, min_row=min_fila+1,max_row=max_fila)
+# Console Clear 
+print("\033[H\033[J", end="")
 
-graficoDeBarras.add_data(data, titles_from_data=True)
-graficoDeBarras.set_categories(categorias)
-graficoDeBarras.title = "Resumen"
-graficoDeBarras.y_axis.title = 'Cant. de Usuarios'
-graficoDeBarras.x_axis.title = 'Modalidad'
-graficoDeBarras.width = 30
-graficoDeBarras.height = 15
-pestaña.add_chart(graficoDeBarras,"B7")
 
-#Formulas de Excel
-pestaña["B5"] = "Recuento"
-i="B"
-columnas = list(string.ascii_uppercase)
-sumas = columnas[0:max_columna]
-for i in sumas:
-    if i!="U":
-        pestaña[f'{i}5'] = f'=SUM({i}3:{i}4'
+# ------------- Configuracion SQL -------------------
+print(" -------- SQL Config Setup -------- ")
 
-#Guarda los Cambios Realizados
-lectura.save('Informe Automatico - Danna Fox.xlsx')
+mydb = mysql.connector.connect(
+  host = "localhost",
+  user = "root",
+  password = "",
+  database = "dannafox-example"
+)
+
+print("\033[H\033[J", end="")
+
+cursor = mydb.cursor()
+nombre = 'Gibson'#input("Ingrese el nombre de la campaña: ")
+# Traer el estado de la campania y comparar
+q1 = "SELECT estado FROM campania WHERE nombre = '"+nombre + "'"
+cursor.execute(q1,)
+estado =  cursor.fetchone()
+estado = str(estado[0])
+excel = ''
+if (estado.upper() != "FINALIZADA" ):
+    print("La Campaña no ha finalizado")
+    quit()
+else:
+  #SELECCIONA LA CAMPAÑA FINALIZADA
+  q2 = "SELECT * FROM campania WHERE nombre = '" + nombre +"'"
+  cursor.execute(q2,)
+  info =  cursor.fetchone()
+  mensaje = info[5]
+
+q5 = "SELECT nombre FROM localidad l INNER JOIN campania_por_localidad lpc WHERE l.id = lpc.localidad_id AND lpc.campania_id = " + str(info[0])
+cursor.execute(q5,)
+localidades = cursor.fetchall() #GUARDA LAS LOCALIDADES DE UNA CAMPAÑA
+
+cant_mensajes = info[5] #GUARDA LA CANTIDAD DE MENSAJES DE UNA CAMPAÑA
+fecha_inicio = info[6] #GUARDA LA FECHA DE INICIO DE UNA CAMPAÑA
+
+print("\033[H\033[J", end="") #Limpia la pantalla
+
+#Lee el archivo excel con la base de datos
+locs = []
+
+for localidad in localidades:
+    locs.append(str(localidad))
+wb = openpyxl.Workbook()
+hoja = wb.active
+# Crea la fila del encabezado con los títulos
+hoja.append(('Nombre De la Campaña', 'Localidades', 'Cantidad de Mensajes', 'Fecha Inicio'))
+producto = [nombre,localidades[0][0],cant_mensajes,fecha_inicio]
+hoja.append(producto)
+
+j=0
+for a in range(len(localidades)):
+  hoja["B"+str(a+2)] = localidades [j][0]
+  j = j+1
+
+
+# producto es una tupla con los valores de un producto 
+
+wb.save('Informe Automatico - Danna Fox.xlsx')
 
 
 #ENVIO DEL REPORTE VIA GMAIL
@@ -53,7 +83,7 @@ from email import encoders
 remitente = 'labdeleng2022@gmail.com' #GMail creado unicamente para la actividad
 destinatarios = ['labdeleng2022@gmail.com']
 asunto = '[INFORME] Informe Actualizado de la Campaña publicitaria finalizada'
-cuerpo = 'Este es un aviso automatico, por favor no responder.\nAtte. Equipo de Desarrollo - Dana Fox'
+cuerpo = 'Este es un aviso automatico, por favor no responder.\nAtte. Equipo de Desarrollo - Danna Fox'
 ruta_adjunto = 'Informe Automatico - Danna Fox.xlsx'
 nombre_adjunto = 'Informe Automatico - Danna Fox.xlsx'
 
@@ -99,3 +129,4 @@ sesion_smtp.sendmail(remitente, destinatarios, texto)
 
 # Cerramos la conexión
 sesion_smtp.quit()
+print("\n\nSe envio el mail a la direccion LdLenguajes@gmail.com\n\n")
