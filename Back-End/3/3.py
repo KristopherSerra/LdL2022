cffd979d19f4d70ef1a8b1d6c87abf10ebded63f
Matias Bue <matias.bue@outlook.com>
2022-11-18 14:50:29 -0300

    cambios en la sentencia sql y agregado control de inputs

--- Back-End/3/3.py
+++ Back-End/3/3.py
@@ -29,71 +29,71 @@ rangos = pd.read_csv(dir)
 cantidad = int(input("Ingrese total de localidades a cargar: "))
 
 # Check para que se ingrese 1 localidad como minimo
-if (cantidad < 1):
-    print("Numero no valido, cerrando...")
-    quit()
-else:
+while (cantidad < 1):
+    print("Numero no valido, ingrese nuevamente...")
+    cantidad = int(input("Ingrese total de localidades a cargar: "))
 
-    for i in range(cantidad):
-        # Creacion del DataFrame con la info de la localidad pedida
+
+for i in range(cantidad):
+    # Creacion del DataFrame con la info de la localidad pedida
+    loc = str(input('Ingrese localidad: ').upper())
+
+    # Chequeo para verificar si la ciudad es valida
+    while(loc not in set(rangos["LOCALIDAD"])):
+        print("No se ha encontrado la localidad ingresada, ingrese nuevamente...")
         loc = str(input('Ingrese localidad: ').upper())
+        
+    location = rangos[rangos['LOCALIDAD'] == loc]
 
-        # Chequeo para verificar si la ciudad es valida
-        if (loc in set(rangos["LOCALIDAD"])): 
-            location = rangos[rangos['LOCALIDAD'] == loc]
+    # Obtener id de la localidad
+    getId = "SELECT id FROM localidad WHERE nombre = %s ".format(loc)
+    values = (loc,)
+    mycursor.execute(getId, values)
+    id = mycursor.fetchone()
+    id = int(id[0])
 
-            # Obtener id de la localidad
-            getId = "SELECT id FROM localidad WHERE nombre = %s ".format(loc)
-            values = (loc,)
-            mycursor.execute(getId, values)
-            id = mycursor.fetchone()
-            id = int(id[0])
 
+
+    # Contienen el conjunto del codigo de area + el bloque
+    codArea = []
+    bloque = [] 
+
+    for i in range(len(location)):
+        codArea.append(str(location.iloc[i].INDICATIVO))
+        bloque.append(str(location.iloc[i].BLOQUE))
+    print("Datos de la locacion cargados exitosamente")    
+
+
+    # Almacena cuantos digitos faltan para completar el numero
+    faltantes = [] 
+    for i in range(len(bloque)):
+        faltantes.append(10 - (len(str(codArea[i])) + len(str(bloque[i]))))
+
+    print("Se han realizado todos los pre-calculos necesarios")
+    sleep(3)
+    print("Iniciando la creacion, aguarde un momento...")
+
+
+    # Crear el numero
+    numeros = []
+    for i in range(len(faltantes)):
+
+        start = int(bloque[i])*(10**faltantes[i])
+        if (int(bloque[i]) >= 100):
+            end = ((int(bloque[i])+1)*(10**faltantes[i]))
         else:
-            print("No se ha encontrado la localidad ingresada, Finalizando...")
-            quit()
-
-
-        # Contienen el conjunto del codigo de area + el bloque
-        codArea = []
-        bloque = [] 
-
-        for i in range(len(location)):
-            codArea.append(str(location.iloc[i].INDICATIVO))
-            bloque.append(str(location.iloc[i].BLOQUE))
-        print("Datos de la locacion cargados exitosamente")    
-
-
-        # Almacena cuantos digitos faltan para completar el numero
-        faltantes = [] 
-        for i in range(len(bloque)):
-            faltantes.append(10 - (len(str(codArea[i])) + len(str(bloque[i]))))
-
-        print("Se han realizado todos los pre-calculos necesarios")
-        sleep(3)
-        print("Iniciando la creacion, aguarde un momento...")
-
-
-        # Crear el numero
-        numeros = []
-        for i in range(len(faltantes)):
-
-            start = int(bloque[i])*(10**faltantes[i])
-            if (int(bloque[i]) >= 100):
-                end = ((int(bloque[i])+1)*(10**faltantes[i]))
-            else:
-                end = ((int(bloque[i])+10)*(10**faltantes[i]))
+            end = ((int(bloque[i])+10)*(10**faltantes[i]))
 
 
 
-            for j in range(start, end):
-                sql = "INSERT INTO telefono (localidad_id, nombre, numero) VALUES (%s,%s , %s)".format(id)
+        for j in range(start, end):
+            sql = "INSERT INTO telefono (localidad_id, numero) VALUES (%s, %s)".format(id)
 
-                num = str(codArea[i]) + str(j)
-                values = (id,loc, num,)
-                mycursor.execute(sql, values)
-                mydb.commit()
+            num = str(codArea[i]) + str(j)
+            values = (id, num,)
+            mycursor.execute(sql, values)
+            mydb.commit()
 
-        print("Numeros cargados correctamente")
+    print("Numeros cargados correctamente")
 
 
